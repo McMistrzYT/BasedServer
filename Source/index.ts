24258e811d914d2d5d62557b11413ee2cc1f84b9
McMistrzYT <56406996+McMistrzYT@users.noreply.github.com>
2023-10-06 19:25:44 +0200

    fun changes

--- Source/index.ts
+++ Source/index.ts
@@ -1,5 +1,21 @@
-import { Msg } from "./Modules/Logger";
 import { config } from "dotenv";
 config();
 
 import "./Handlers/Server";
+
+/*
+    Welcome to Mc's BasedServer template!
+    This is the exact same base I use for projects like Dispriv, Birdnest, GDTS and more.
+
+    Here's a quick overview:
+        - Handlers:
+          Handlers are the big boys - websockets, servers, anything that has to be constantly running.
+          Personally, I always import those here in index, like the Server handler.
+        - Modules:
+          These are smaller, util classes. A good example of that is the Logger util, which lets you
+          print to the console with COLORS!
+        - Routes:
+          The routes folder houses scripts with a router, some endpoints and other fun stuff!
+
+    I recommend familiarizing yourself with all the code before adding anything yourself.
+*/
